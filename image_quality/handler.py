748729fc313e0e47d4dbf71a65bdb7a97d893baa
Krishna Kalyan <krishna.kalyan@zoi.de>
2018-05-17 19:19:56 +0200

    more code

--- image_quality/handler.py
+++ image_quality/handler.py
@@ -1,8 +1,19 @@
 import random
-
+import urllib
+import cv2
 
 def context(event, context):
-    print(event)
-    print(context)
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+
+        print(key)
+
+
+        #raw_key = urllib.parse.unquote_plus(key)
+        download_path = '/tmp/{}'.format(key)
+        upload_path = '/tmp/resized-{}'.format(key)
+
+
 
     return 'Hello World'
